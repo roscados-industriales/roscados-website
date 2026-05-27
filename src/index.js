b2550fc76cc5a409a802c3c3dd94c042ab64df63
roscados-industriales <arturo.ferrino.gtz@gmail.com>
2026-05-27 13:57:08 -0600

    Create index.js

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import RoscadosWebsite from './RoscadosWebsite';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RoscadosWebsite />
+  </React.StrictMode>
+);
